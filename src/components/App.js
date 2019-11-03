57b9d8d1504c04fc8084a7fba32d0afa0d62ba6e
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2019-11-03 21:39:28 +0100

    feat(component): add basic component structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
-function App() {
-  return (
-    <h1>Chuck Norris Jokes API</h1>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
